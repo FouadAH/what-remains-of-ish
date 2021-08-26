f52eb6be8955ea05598c32dc2231838e4d00362f
FouadAH <38855408+FouadAH@users.noreply.github.com>
2021-08-26 16:36:21 +0300

    Ruin levels reworked

--- Assets/Scripts/Triggers/LoadTrigger.cs
+++ Assets/Scripts/Triggers/LoadTrigger.cs
@@ -8,12 +8,29 @@ public class LoadTrigger : MonoBehaviour
     [SerializeField] private int levelToLoad;
     [SerializeField] private int levelToUnload;
 
+    public GameObject playerPos;
+
+    private void Start()
+    {
+        
+    }
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.tag.Equals("Player"))
         {
             Debug.Log("Loading Level:" + levelToLoad);
-            GameManager.instance.LoadScene(levelToUnload, levelToLoad);
+            GameManager.instance.LoadScene(levelToUnload, levelToLoad, playerPos.transform.position);
         }
     }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawWireSphere(playerPos.transform.position, 1f);
+        BoxCollider2D boxCollider2D = GetComponent<BoxCollider2D>();
+        Vector3 boxPos = boxCollider2D.transform.position;
+        Vector2 vector = new Vector2(boxPos.x + boxCollider2D.offset.x, boxPos.y + boxCollider2D.offset.y);
+        Gizmos.DrawWireCube(vector, GetComponent<BoxCollider2D>().size);
+    }
 }
+
+
