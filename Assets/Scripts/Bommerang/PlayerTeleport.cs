dbb06d654c8ad9d721425922da9fb6cc8cd18299
Mika Zahar <zaharmika24@gmail.com>
2021-12-27 11:25:23 +0400

    Added Boomerang Teleport SFX

--- Assets/Scripts/Bommerang/PlayerTeleport.cs
+++ Assets/Scripts/Bommerang/PlayerTeleport.cs
@@ -63,6 +63,7 @@ public class PlayerTeleport : MonoBehaviour
 
             StartCoroutine(TeleportEffect(teleporEffectDuration));
             StartCoroutine(Teleport(transformToMove, teleportDelay));
+            FMODUnity.RuntimeManager.PlayOneShot("event:/SFX/Player/Dash", GetComponent<Transform>().position);
         }
     }
 
